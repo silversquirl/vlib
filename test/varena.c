28c7d0e1fe2c0ec3e67e14c431ba0ef1e588f6bf
Samadi van Koten <samadi@vktec.org.uk>
2020-07-22 00:18:32 +0100

    [varena] Add unit test

---
+++ test/varena.c
@@ -0,0 +1,51 @@
+#define VARENA_IMPL
+#include "vtest.h"
+#include "../varena.h"
+
+VTEST(test_allocation) {
+	struct varena *arena = varena_new();
+	vassert_not_null(arena);
+
+	vassert_not_null(aalloc(&arena, 100));
+
+	varena_free(arena);
+}
+
+VTEST(test_large_allocation) {
+	struct varena *arena = varena_new();
+	vassert_not_null(arena);
+
+	vassert_null(aalloc(&arena, 100000));
+
+	varena_free(arena);
+}
+
+VTEST(test_multiple_allocations) {
+	struct varena *arena = varena_new();
+	vassert_not_null(arena);
+
+	for (int i = 0; i < 100; i++) {
+		vassert_not_null(aalloc(&arena, 100));
+	}
+
+	varena_free(arena);
+}
+
+VTEST(test_multiple_large_allocations) {
+	struct varena *arena = varena_new();
+	vassert_not_null(arena);
+
+	for (int i = 0; i < 100; i++) {
+		vassert_null(aalloc(&arena, 100000));
+		vassert_not_null(aalloc(&arena, 100));
+	}
+
+	varena_free(arena);
+}
+
+VTESTS_BEGIN
+	test_allocation,
+	test_large_allocation,
+	test_multiple_allocations,
+	test_multiple_large_allocations,
+VTESTS_END
